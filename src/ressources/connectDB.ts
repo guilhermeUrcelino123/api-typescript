be955dbb05a7e98928747623beca179ff58d1ba9
DESKTOP-89A8OS7\guilh <gui.urcelino@gmail.com>
2022-03-14 14:40:47 -0300

    mudando url banco

--- src/ressources/connectDB.ts
+++ src/ressources/connectDB.ts
@@ -1,8 +1,8 @@
 import {connect} from 'mongoose';
 
-const URL_DB:string = "mongodb://localhost:27017/bancoTeste";
+//const URL_DB:string = "mongodb://localhost:27017/bancoTeste";
 
-//const URL_DB:string = "mongodb+srv://gui:123@cluster0.hkxpt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const URL_DB:string = "mongodb+srv://gui:123@cluster0.hkxpt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 function connectDB() {
     connect(URL_DB,)
